84d836b59af429ddd9842d1bb94f769f68da7f85
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:36:01 +0200

    tb_rhs_aftermath.desert_island mission added

---
+++ briefing_red.hpp
@@ -0,0 +1,11 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<br />
+<br />
+
+<titleFont>NUCLEAR WAR COULD HAPPEN TOMORROW!</titleFont><br /><br /><br />
+<titleFont>THE GOOD NEWS IS  YOU MIGHT BE KILLED.</titleFont><br />
+<br />
+<titleFont>THE BAD NEWS IS YOU MIGHT SURVIVE!</titleFont><br />
+
